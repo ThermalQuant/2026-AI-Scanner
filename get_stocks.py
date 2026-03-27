d363023b49d4fb2b4c72da6c26e5a903ca2bb94b
ThermalQuant <donnaball9999@gmail.com>
2026-03-26 20:31:18 -0400

    Update get_stocks.py

--- get_stocks.py
+++ get_stocks.py
@@ -2,34 +2,32 @@ import pandas as pd
 import yfinance as yf
 import os
 
-# 1. THE TICKERS (S&P 500 + YOUR CORE LIST)
-# You can add all 500 here later, but let's start with these to ensure it runs!
-tickers = ["AAPL", "MSFT", "NVDA", "TSLA", "GOOGL", "AMZN", "META", "APLD", "PLTR", "VRT"] 
+# 1. THE COMPLETE S&P 500 LIST (Wakes up MMM and the rest)
+tickers = ["MMM", "AOS", "ABT", "ABBV", "ACN", "ADBE", "AMD", "AES", "AFL", "A", "APD", "AKAM", "ALB", "ARE", "ALGN", "ALLE", "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AMCR", "AEE", "AAL", "AEP", "AXP", "AIG", "AMT", "AWK", "AMP", "AME", "AMGN", "APH", "ADI", "ANSS", "AON", "APA", "AAPL", "AMAT", "APTV", "ACGL", "ADM", "ANET", "AJG", "AIZ", "T", "ATO", "ADSK", "ADP", "AZO", "AVB", "AVY", "AXON", "BKR", "BALL", "BAC", "BK", "BBWI", "BAX", "BDX", "WRB", "BRK-B", "BBY", "BIO", "TECH", "BIIB", "BLK", "BX", "BA", "BKNG", "BWA", "BXP", "BSX", "BMY", "AVGO", "BR", "BRO", "BF-B", "BLDR", "BG", "CDNS", "CZR", "CPT", "CPB", "COF", "CAH", "KMX", "CCL", "CARR", "CTLT", "CAT", "CBOE", "CBRE", "CDW", "CE", "COR", "CNC", "CNP", "CF", "CHRW", "CRL", "SCHW", "CHTR", "CVX", "CMG", "CB", "CHD", "CI", "CINF", "CTAS", "CSCO", "C", "CFG", "CLX", "CME", "CMS", "KO", "CTSH", "CL", "CMCSA", "CMA", "CAG", "COP", "ED", "STZ", "CEG", "COO", "CPRT", "GLW", "CPAY", "CTVA", "CSGP", "COST", "CTRA", "CCI", "CSX", "CMI", "CVS", "DHR", "DRI", "DVA", "DE", "DAL", "XRAY", "DVN", "DXCM", "FANG", "DLR", "DFS", "DG", "DLTR", "D", "DPZ", "DOV", "DOW", "DHI", "DTE", "DUK", "DD", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "ELV", "LLY", "EMR", "ENPH", "ETR", "EOG", "EPAM", "EQT", "EFX", "EQIX", "EQR", "ESS", "EL", "ETSY", "EG", "EVRG", "ES", "EXC", "EXPE", "EXPD", "EXR", "XOM", "FFIV", "FICO", "META", "FDX", "FITB", "FSLR", "FE", "FI", "FMC", "F", "FTNT", "FTV", "FOXA", "FOX", "BEN", "FCX", "GRMN", "IT", "GE", "GEHC", "GEV", "GEN", "GNRC", "GD", "GIS", "GM", "GPC", "GILD", "GPN", "GL", "GS", "HAL", "HIG", "HAS", "HCA", "PEAK", "HSIC", "HSY", "HES", "HPE", "HLT", "HOLX", "HD", "HON", "HRL", "HST", "HWM", "HPQ", "HUM", "HBAN", "HII", "IBM", "IEX", "IDXX", "ITW", "ILMN", "INCY", "IR", "PODD", "INTC", "ICE", "IFF", "INTU", "ISRG", "IVZ", "INVH", "IQV", "IRM", "JBHT", "JBL", "JKHY", "JJSF", "J", "SJM", "JPM", "JNPR", "K", "KVUE", "KDP", "KEY", "KEYS", "KMB", "KIM", "KPC", "KMI", "KLAC", "KHC", "KR", "LHX", "LH", "LRCX", "LW", "LVS", "LDOS", "LEN", "LIN", "LYV", "LKQ", "LMT", "L", "LOW", "LULU", "LYB", "MTB", "MRO", "MPC", "MKTX", "MAR", "MMC", "MLM", "MAS", "MA", "MTCH", "MKC", "MCD", "MCK", "MDT", "MRK", "MET", "MTD", "MGM", "MCHP", "MU", "MSFT", "MAA", "MHK", "MOH", "TAP", "MDLZ", "MPWR", "MNST", "MCO", "MS", "MOS", "MSI", "MSCI", "NDAQ", "NTAP", "NFLX", "NEM", "NWL", "NFX", "NEE", "NKE", "NI", "NDSN", "NSC", "NTRS", "NOC", "NCLH", "NRG", "NUE", "NVDA", "NVR", "NXPI", "ORLY", "OXY", "ODFL", "OMC", "ON", "OKE", "ORCL", "OTIS", "PCAR", "PKG", "PANW", "PARA", "PH", "PAYX", "PAYC", "PYPL", "PNR", "PEP", "PFE", "PCG", "PM", "PSX", "PNW", "PXD", "PNC", "POOL", "PPG", "PPL", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PTC", "PSA", "PHM", "QRVO", "PWR", "QCOM", "DGX", "RL", "RJF", "RTX", "O", "REG", "REGN", "RF", "RSG", "RMD", "RVTY", "RHI", "ROK", "ROL", "ROP", "ROST", "RCL", "SPGI", "CRM", "SBAC", "SLB", "STX", "SRE", "NOW", "SHW", "SPG", "SWKS", "SNA", "SO", "LUV", "SWK", "SBUX", "STT", "STV", "STE", "SYK", "SYF", "SNPS", "SYY", "TMUS", "TROW", "TTWO", "TPR", "TRGP", "TGT", "TEL", "TDY", "TFX", "TER", "TSLA", "TXN", "TXT", "TMO", "TJX", "TSCO", "TT", "TDG", "TRV", "TRU", "TFC", "TYL", "TSN", "USB", "UDR", "ULTA", "UNP", "UAL", "UPS", "URI", "UNH", "UHS", "VLO", "VTR", "VRSN", "VRSK", "VZ", "VRTX", "VFC", "VCI", "V", "VMC", "VICI", "WBA", "WMT", "WBD", "WM", "WAT", "WEC", "WFC", "WELL", "WST", "WDC", "WRK", "WY", "WHR", "WMB", "WTW", "WYNN", "XEL", "XYL", "YUM", "ZBRA", "ZBH", "ZTS"]
 
-# 2. Download Data
-raw_data = yf.download(tickers, period="1mo", group_by='ticker')
+# 2. DOWNLOAD DATA
+raw_data = yf.download(tickers, period="5d", interval="1d", group_by='ticker')
 
 processed_list = []
 for ticker in tickers:
-    df = raw_data[ticker].copy()
-    df['Ticker'] = ticker
-    
-    # CALCULATE: Daily % Change
-    df['Daily_Change_%'] = df['Close'].pct_change() * 100
-    
-    # CALCULATE: Volume Intensity
-    df['Vol_Intensity'] = df['Volume'] / df['Volume'].rolling(window=5).mean()
-    
-    # NEW: CALCULATE SIGNAL (1 = BUY, -1 = SELL, 0 = WAIT)
-    df['Signal'] = 0
-    df.loc[(df['Vol_Intensity'] > 1.2) & (df['Daily_Change_%'] > 0), 'Signal'] = 1
-    df.loc[(df['Vol_Intensity'] > 1.2) & (df['Daily_Change_%'] < 0), 'Signal'] = -1
+    try:
+        df = raw_data[ticker].copy()
+        
+        # BASIC STATS
+        df['Ticker'] = ticker
+        df['Daily_Change_%'] = df['Close'].pct_change() * 100
+        df['Vol_Intensity'] = df['Volume'] / df['Volume'].rolling(window=5).mean()
+        
+        # SIGNAL LOGIC
+        df['Signal'] = 0
+        df.loc[(df['Vol_Intensity'] > 1.2) & (df['Daily_Change_%'] > 0), 'Signal'] = 1
+        df.loc[(df['Vol_Intensity'] > 1.2) & (df['Daily_Change_%'] < 0), 'Signal'] = -1
+        
+        # Keep only the most recent day
+        processed_list.append(df.tail(1))
+    except:
+        continue
 
-    processed_list.append(df.tail(1)) # Only keep the latest data point
-
-# 3. Save the File
+# 3. SAVE TO CSV
 final_df = pd.concat(processed_list)
-file_path = 'MarketData_Pro.csv'
-final_df.to_csv(file_path, index=False)
-
-print("SUCCESS: Intensity and Signals Calculated.")
+final_df.to_csv('MarketData_Pro.csv', index=False)
